5d7071ad5b73a3404b2dfcc9225f3b2717ae679d
joed <ezalorsara@gmail.com>
2020-10-04 08:50:03 +0800

    test

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -3,9 +3,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   if (_req.method === "POST") {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json({ content: "post ok" });
+    res.status(200).json({ status: 200, message: "post ok" });
   } else {
-    res.status(200).json({ content: "get ok" });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ status: 200, message: "get ok" });
   }
 };
 
