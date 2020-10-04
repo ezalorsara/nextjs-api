779b1b6f995087d3e475c538ed6a91599d39f44f
joed <ezalorsara@gmail.com>
2020-10-04 17:15:18 +0800

    test

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   if (_req.method === "POST") {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(_req);
+    res.status(200).json({ headers: JSON.stringify(_req.headers) });
   } else {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(_req);
